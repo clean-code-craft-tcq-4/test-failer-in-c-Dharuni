1c117c499ab8072fdb63c108baf0e7ca0fd5ec42
Belavatagi-Dharuni <34068379+Dharuni@users.noreply.github.com>
2022-06-21 21:38:00 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -6,6 +6,7 @@ const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
 int numberOfMajorColors = sizeof(majorColor) / sizeof(majorColor[0]);
+int numberOfMinorColors =  sizeof(minorColor) / sizeof(minorColor[0]);
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
