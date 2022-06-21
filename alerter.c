ea6ca2b68e61e2f03eb24711874f80aedece9211
Belavatagi-Dharuni <34068379+Dharuni@users.noreply.github.com>
2022-06-21 22:14:01 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -3,6 +3,9 @@
 
 int alertFailureCount = 0;
 
+#define PRODUCTION_ON   1
+#define PRODUCTION_ENABLED PRODUCTION_ON
+
 int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
     // Return 200 for ok
@@ -11,9 +14,16 @@ int networkAlertStub(float celcius) {
     return 200;
 }
 
+#if(PRODUCTION_ENABLED != PRODUCTION_ON)
+void testalertInCelcius(float celcius, int returnCode) {
+    assert(celcius<returnCode);
+}  
+#endif
+
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlertStub(celcius);
+#if(PRODUCTION_ENABLED == PRODUCTION_ON)   
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
@@ -21,6 +31,9 @@ void alertInCelcius(float farenheit) {
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
+#else
+    testalertInCelcius(celcius, returnCode);
+#endif
 }
 
 int main() {
